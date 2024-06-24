41f1a44f024f527f517a301278b0f71460806ada
Willjorn <klpumagoi@gmail.com>
2024-06-24 17:45:27 -0500

    finished

--- candidate testing_v1.js
+++ candidate testing_v1.js
@@ -9,12 +9,12 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-/*
+
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
 let candidateAnswers;
-*/
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -25,7 +25,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question);
 }
-/*
+
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   if (candidateAnswer === correctAnswer) {
@@ -37,15 +37,15 @@ function gradeQuiz(candidateAnswers) {
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
   return grade;
 }
-*/
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-  console.log("Hello " + candidateName + "!");
+  console.log("Hello" + candidateName + "!");
   askQuestion();
-  //gradeQuiz(candidateAnswers);
+  gradeQuiz(candidateAnswers);
 };
-/*
+
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
@@ -59,4 +59,3 @@ module.exports = {
   runProgram: runProgram
 }
 ;
-*/
